ca5997d015aec993ca74e31de8b92ec32b92aee2
Fahim Shariff <52634330+Fahim-at-work@users.noreply.github.com>
2020-01-21 22:22:56 -0500

    Add files via upload

---
+++ tic_tac_toe.py
@@ -0,0 +1,22 @@
+# creating the board
+board = ["-", "-", "-",
+         "-", "-", "-",
+         "-", "-", "-"]
+
+def display_board():
+    print(board[0] + " | " + board[1] + " | " + board[2])
+    print(board[3] + " | " + board[4] + " | " + board[5])
+    print(board[6] + " | " + board[7] + " | " + board[8])
+
+
+#setting the game to true and winner will be none when the game starts
+game_is_on = True
+winner = None
+
+#setting the players and taking their names
+print('Player one will be X and player two will be O ')
+player_one = input('Player one enter your name: ')
+player_two = input('Player two enter your name: ')
+
+#setting the current player to player one that is X
+current_player = player_one
